07cdee386cd3780bec6f96c847d1e90cfac80fe9
tongWz <tongweizhen@foxmail.com>
2022-12-29 10:32:54 +0800

    初始化

---
+++ src/DoIt.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: tongWZ
+ * Date: 2022/12/29
+ * Time: 10:24
+ */
+namespace Tongwz\Composers;
+
+class DoIt
+{
+    public static function name()
+    {
+        echo "My name is tongWz";
+    }
+}
